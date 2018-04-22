b83f09d37e48d464b25996d7db25bfd1c1e1c065
YutoAoki <frcsnt@gmail.com>
2018-04-22 15:46:14 +0900

    ログイン機能まで実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sessions/new'
+
+  resources 'users'
 
   get 'pages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 
 end
